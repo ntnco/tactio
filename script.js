8a4d8331c9fd7c7199443134dcee34dd379ccc2f
System Administrator <root@Mahers-MacBook-Pro.local>
2020-01-25 21:30:10 -0500

    calc average

--- script.js
+++ script.js
@@ -16,4 +16,13 @@ $(document).ready(function()
             }
         });
     });
+
+    function numAverage(a) {
+        var b = a.length,
+            c = 0, i;
+        for (i = 0; i < b; i++){
+          c += Number(a[i]);
+        }
+        return c/b;
+      }
 });
