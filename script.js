4a9558fad0333b071fe58eeef833d4bfe289312f
ntnco <antoinefcolson@gmail.com>
2020-01-26 00:25:22 -0500

    removing my commit

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ $(document).ready(function()
     $('#headingOne').click(function(){
         $.ajax({
             url : 'https://sandbox030.tactiorpm7000.com/tactio_clinical_restful_api.php/1.1.5/Observation?subject=517d8e98-3d63-11ea-b585-000d3af3ac45',
-            type : 'GET',
+            type : "GET",
             dataType:'jsonp',
             beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "Bearer 5934c4001fc23206f90fec5b8a6f5c7deb4a9833"); 
-             },
+            xhr.setRequestHeader("Authorization", "Bearer 5934c4001fc23206f90fec5b8a6f5c7deb4a9833"); 
+            },
             success: function(result) { 
-                alert('Successfully called');
+                console.log(result);
             },
             error: function(jqxhr, status, exception) {
                 console.log(exception);
@@ -21,14 +21,14 @@ $(document).ready(function()
         var b = a.length,
             c = 0, i;
         for (i = 0; i < b; i++){
-          c += Number(a[i]);
+            c += Number(a[i]);
         }
         return c/b;
-      }
+    }
 });
 
 
-var patients = {
+let patients = {
     "11": [
         {
             "hr": 136,  
@@ -45,14 +45,14 @@ var patients = {
             "tension": [95, 130],
             "date": "2019-01-21"
         }
-    ];
-    "12": "...";
-    "13": "...";
-    "14": "...";
-    "15": "...";
-    "16": "...";
-    "17": "...";
-    "18": "...";
-    "19": "...";
-    "20": "...";
+    ],
+    "12": "...",
+    "13": "...",
+    "14": "...",
+    "15": "...",
+    "16": "...",
+    "17": "...",
+    "18": "...",
+    "19": "...",
+    "20": "...",
 }
