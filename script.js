3567ca3210f779e1a4b0e91b99046b23461fc0ea
System Administrator <root@Mahers-MacBook-Pro.local>
2020-01-25 21:18:45 -0500

    ajoute chart

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ $(document).ready(function()
             url : 'https://sandbox030.tactiorpm7000.com/tactio_clinical_restful_api.php/1.1.5/Observation?subject=517d8e98-3d63-11ea-b585-000d3af3ac45',
             type : 'GET',
             dataType:'jsonp',
-            crossDomain: true,
             beforeSend: function(xhr) {
                 xhr.setRequestHeader("Authorization", "Bearer 5934c4001fc23206f90fec5b8a6f5c7deb4a9833"); 
              },
