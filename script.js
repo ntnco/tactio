203dc59f04d0b17208429cde10eb63ef7d6b90b2
System Administrator <root@Mahers-MacBook-Pro.local>
2020-01-26 00:19:09 -0500

    curl

--- script.js
+++ script.js
@@ -2,14 +2,9 @@ $(document).ready(function()
 {
     $('#headingOne').click(function(){
         $.ajax({
-            url : 'https://sandbox030.tactiorpm7000.com/tactio_clinical_restful_api.php/1.1.5/Observation?subject=517d8e98-3d63-11ea-b585-000d3af3ac45',
-            type : 'GET',
-            dataType:'jsonp',
-            beforeSend: function(xhr) {
-                xhr.setRequestHeader("Authorization", "Bearer 5934c4001fc23206f90fec5b8a6f5c7deb4a9833"); 
-             },
+            url : 'curl.php',
             success: function(result) { 
-                alert('Successfully called');
+                console.log(result);
             },
             error: function(jqxhr, status, exception) {
                 console.log(exception);
