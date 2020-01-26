805e0a5af87a0030e493a3ce2c029c69492c5191
ntnco <antoinefcolson@gmail.com>
2020-01-26 04:22:09 -0500

    update the js

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 
 
 $(document).ready(function() {
-
-    
-
+    let tags = ["Jan 4th", "Jan 11th", "Jan 18th", "Jan 25th"];
     $.ajax({
         url : 'curl.php',
         success: function(result) { 
-            let rawInfos = getArray(result);
-            let neatInfos = neatify(rawInfos);
+            let rawInfos = getArray(result),
+                neatInfos = neatify(rawInfos);
+            //data.labels = ["tiger", "elephant", "koala"];
             console.log(neatInfos.bloodPressure);
             console.log(neatInfos.heartRate);
         },
@@ -16,7 +15,43 @@ $(document).ready(function() {
             console.log(exception);
         }
     });
-
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: tags,
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
 });
 
 
